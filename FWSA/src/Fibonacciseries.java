f044020bc022fdc48f52fd335091e5c44a792415
kasi-antony <antokasi44@gmail.com>
2022-04-25 18:28:39 +0530

    added some changes

---
+++ FWSA/src/Fibonacciseries.java
@@ -0,0 +1,15 @@
+public class Fibonacciseries {
+    public static void main(String[] args){
+        int num=12;
+        int a=0;
+        int b=1;
+        int c;
+        for (int i=0; i<=num; i++){
+            System.out.println(a);
+            c=a+b;
+            a=b;
+            b=c;
+        }
+    }
+
+}
---
+++ FWSA/src/Referencedatatypes.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Referencedatatypes {
+    public static void main(String[] args) {
+        //strings practice
+        String name="kasi";
+        String last_name="antony";
+
+        System.out.println(name+" "+last_name);
+
+        //arrays practice
+        String kasi[]={"1","kasi"};
+        String antony[]={"2","antony"};
+
+        System.out.println(Arrays.toString(kasi)+" "+Arrays.toString(antony));
+    }
+
+}
