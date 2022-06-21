d069945440529e7cc8afdfb6dc168d9d38b62c6f
kasi-antony <antokasi44@gmail.com>
2022-06-21 11:54:59 +0530

    added some changes

---
+++ Ruby_practice/casestatement.rb
@@ -0,0 +1,13 @@
+#case statemment
+#example
+fruit = gets.chomp
+case fruit
+when 'orange'
+    puts "I love orange"
+when 'mango'
+    puts "I love mango"
+when 'banana'
+    puts "I love banana"
+else
+    puts "I don't like fruits"
+end
