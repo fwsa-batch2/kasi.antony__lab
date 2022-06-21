c0532b71eb48c2bcf11bc3dbf6a6c3d293c3c9bf
kasi-antony <antokasi44@gmail.com>
2022-06-21 11:45:49 +0530

    added some changes

---
+++ Ruby_practice/if-elsifstatement.rb
@@ -0,0 +1,10 @@
+#if-else-if statement
+#example
+mark =gets.chomp.to_i
+if mark<=25
+    puts "You're fail" 
+elsif (mark>25 && mark<=35)
+    puts "You're just pass"
+else mark
+    puts "You're A1"   
+end
